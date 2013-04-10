9528abbef4a2f64cc0512b342aa30832fd4982af
Ryan Winograd <ryan@thewinograds.com>
2013-04-10 15:50:02 -0500

    Remove puts

--- lib/capistrano-unicorn-init.rb
+++ lib/capistrano-unicorn-init.rb
@@ -1,3 +1,3 @@
 require 'capistrano'
 
-Dir.glob(File.join(File.dirname(__FILE__), '..', '/recipes/*.rb')).sort.each { |f| puts f; load f }
+Dir.glob(File.join(File.dirname(__FILE__), '..', '/recipes/*.rb')).sort.each { |f| load f }
